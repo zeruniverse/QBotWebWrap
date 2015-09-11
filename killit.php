ff5e515274a301e8e8f958d4f9bdf156ff405fe9
Jeffery Zhao <zzy8200@gmail.com>
2015-09-11 12:20:12 -0700

    Create killit.php

---
+++ killit.php
@@ -0,0 +1,11 @@
+<?php
+require_once('function/sqllink.php');
+if(!isset($_POST['id'])) die('{"retcode":999,"msg":"CAN NOT FIND ID IN THE PARAMETER"}');
+$link=sqllink();
+if(!$link) die('{"retcode":99,"msg":"DATABASE ERROR"}');
+$res=sqlexec('SELECT * FROM `process` where `sid`=?',array($_POST['id']),$link);
+$result=$res->fetch(PDO::FETCH_ASSOC);
+if ($result==FALSE)  die('{"retcode":0,"msg":"SUCC"}'); //don't tell malicious person the id does not exist
+shell_exec("kill "+$result['pid']);
+die('{"retcode":0,"msg":"SUCC"}');
+?>
